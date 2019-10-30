9de4f7ce6beda33b7457b9adee32ae3a673aabec
Shitesh Arihunta <arihunta@gmail.com>
2019-10-30 22:46:57 +0200

    Initial commit: day 01

---
+++ src/main/headers/app.h
@@ -0,0 +1,19 @@
+#ifndef APP_H
+#define APP_H
+
+#include <string>
+
+namespace AdventOfCode2016 {
+
+    class Answers {
+
+        public:
+
+            int _01_01();
+            int _01_02();
+
+    };
+
+}
+
+#endif
---
+++ src/test/cpp/app_test.cpp
@@ -0,0 +1,14 @@
+#include "app.h"
+#include <cassert>
+#include <iostream>
+
+int main() {
+
+    AdventOfCode2016::Answers greeter;
+
+    std::cout << "01-01: " << greeter._01_01() << std::endl;
+    std::cout << "01-02: " << greeter._01_02() << std::endl;
+
+    return 0;
+
+}
